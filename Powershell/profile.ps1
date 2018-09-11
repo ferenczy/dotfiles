b8de54dd51aa62e69c9a702c71e1ac100bb1b3fd
Dawid Ferenczy <dawid@ferenczy.cz>
2018-09-11 20:59:56 +0200

    Added missing closing curly bracket

--- Powershell/profile.ps1
+++ Powershell/profile.ps1
@@ -21,6 +21,7 @@ Import-Module posh-git
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
+}
 
 
 # - - - configure modules - - -
