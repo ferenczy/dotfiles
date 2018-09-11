aadfde9436cc32dd1be9bfa24ed59ce286768ba0
Dawid Ferenczy <dawid@ferenczy.cz>
2018-09-11 20:35:15 +0200

    Changed prompt function to be explicitly global

--- Powershell/profile.ps1
+++ Powershell/profile.ps1
@@ -22,7 +22,8 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 
-function prompt {
+# - - - define functions - - -
+function global:prompt {
     Write-Host
     Write-Host ("[" + $(Get-Date -UFormat "%Y-%m-%d %H:%M.%S") + "] ") -nonewline -ForegroundColor DarkCyan
     Write-Host $env:username -nonewline -ForegroundColor Cyan
